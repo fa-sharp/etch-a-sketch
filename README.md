ff00865e821846fcd6aa6f881a7af0127cd96b0e
farshad <farshadpiano@gmail.com>
2021-07-10 03:40:20 -0500

    Using PointerMove event for mobile use

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # Etch A Sketch
 
+[Live demo!](https://fa-sharp.github.io/etch-a-sketch/)
+
 Assignment from the [Odin Project](https://www.theodinproject.com/paths/foundations/courses/foundations/lessons/etch-a-sketch-project)
 - User can sketch in the grid using their mouse
 - Grid has a fixed visual size, but user can customize the number of cells up to 100*100
-- Vanilla Javascript and CSS
+- Plain Javascript and CSS
 
 Thanks to [this StackOverflow answer](https://stackoverflow.com/a/43687046) from Michael Benjamin, for helping 
 me figure out how to set up CSS Grid with collapsing borders :)
--- etch-script.js
+++ etch-script.js
@@ -3,8 +3,13 @@ const etchGridElement = document.querySelector(".etch-grid");
 const resetGridButton = document.querySelector("#resetGridButton");
 
 /** Mouse event listener for filling in the cells */
-const onEtchGridMouseOver = (event) => {
-    let { target } = event;
+const onEtchGridPointerMove = (event) => {
+    if (event.buttons !== 1) // check if primary mouse button is clicked
+        return;
+    
+    // search for the cell using coordinates (pointermove event won't update the target element as you move across the grid)
+    const target = document.elementFromPoint(event.clientX,event.clientY);
+
     if (target.classList.contains("etch-cell")) // check if target element is a cell
         target.classList.add("filled");
 }
@@ -13,13 +18,14 @@ const onEtchGridMouseOver = (event) => {
 const createEtchGrid = (size) => {
 
     // validate size
+    size = Number.parseInt(size);
     if (size < 1 || size > 100) {
         console.error("Invalid grid size!");
         return;
     }
 
-    // reset etch grid (using childNodes.forEach() doesn't work because childNodes a live collection)
-    Array.from(etchGridElement.childNodes).forEach(child => child.remove())
+    // reset etch grid (using childNodes.forEach() directly doesn't work because childNodes is a live collection)
+    Array.from(etchGridElement.childNodes).forEach(child => child.remove());
 
     // set up CSS Grid
     etchGridElement.style.gridTemplateColumns = `repeat(${size},1fr)`;
@@ -32,15 +38,18 @@ const createEtchGrid = (size) => {
         etchGridElement.appendChild(etchCell);
     }
 
-    // set up event listener (remove the old listener first, if it exists)
-    etchGridElement.removeEventListener("mouseover", onEtchGridMouseOver);
-    etchGridElement.addEventListener("mouseover", onEtchGridMouseOver);
+    // set up mouse event listeners (remove the old listeners first, if needed)
+    etchGridElement.removeEventListener("pointerdown", onEtchGridPointerMove);
+    etchGridElement.removeEventListener("pointermove", onEtchGridPointerMove);
+    etchGridElement.addEventListener("pointerdown", onEtchGridPointerMove);
+    etchGridElement.addEventListener("pointermove", onEtchGridPointerMove);
 }
 
 // Adding event listener for reset button
 resetGridButton.addEventListener("click", () => {
     let newGridSize = prompt("New grid size (limit 100): ", "32");
-    createEtchGrid(newGridSize);
+    if (newGridSize)
+        createEtchGrid(newGridSize);
 });
 
 // Creating the etch grid on page load
